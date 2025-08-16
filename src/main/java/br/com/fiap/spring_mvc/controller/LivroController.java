41eb385158df6782e78f8e257d70f46b2eb9e0cf
Ribaas <ribasleonardo92@gmail.com>
2025-08-15 22:46:35 -0300

    Aula 12/08

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -1,29 +1,48 @@
 package br.com.fiap.spring_mvc.controller;
 
+import br.com.fiap.spring_mvc.entity.Categoria;
 import br.com.fiap.spring_mvc.entity.Livro;
+import br.com.fiap.spring_mvc.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
+
+    @Autowired
+    LivroService livroService;
+
     @GetMapping("/lista")
     public String listarLivros(Model model) {
-        Livro livro1 = new Livro();
-        livro1.setId(1L);
-        livro1.setTitulo("Harry Potter");
-        Livro livro2 = new Livro();
-        livro2.setId(2L);
-        livro2.setTitulo("Percy Jackson");
-        Livro livro3 = new Livro();
-        livro3.setId(3L);
-        livro3.setTitulo("O diário de um banana");
-        List<Livro> livros = List.of(livro1, livro2, livro3);
+        List<Livro> livros = livroService.readLivros();
         model.addAttribute("listaLivros", livros);
         return "livroLista";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastroLivros(Model model) {
+        List< Map<String, Object>> opcoes = new ArrayList<>();
+        Arrays.stream(Categoria.values()).forEach(categoria -> {
+            Map<String, Object> opcao = new HashMap<>();
+            opcao.put("label", categoria.getDescricao());
+            opcao.put("value", categoria);
+            opcoes.add(opcao);
+        });
+        model.addAttribute("opcoes", opcoes);
+        model.addAttribute("livro", new Livro());
+        return "livroCadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarLivro (Livro livro, Model model) {
+        livroService.createLivro(livro);
+        return listarLivros(model);
     }
 }
