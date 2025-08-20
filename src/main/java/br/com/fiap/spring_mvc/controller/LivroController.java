e06731cbb19da790326323d60520093c487aa7e4
Ribaas <ribasleonardo92@gmail.com>
2025-08-19 22:41:24 -0300

    aula 19/08

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -3,9 +3,11 @@ package br.com.fiap.spring_mvc.controller;
 import br.com.fiap.spring_mvc.entity.Categoria;
 import br.com.fiap.spring_mvc.entity.Livro;
 import br.com.fiap.spring_mvc.service.LivroService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,20 +30,18 @@ public class LivroController {
 
     @GetMapping("/cadastro")
     public String cadastroLivros(Model model) {
-        List< Map<String, Object>> opcoes = new ArrayList<>();
-        Arrays.stream(Categoria.values()).forEach(categoria -> {
-            Map<String, Object> opcao = new HashMap<>();
-            opcao.put("label", categoria.getDescricao());
-            opcao.put("value", categoria);
-            opcoes.add(opcao);
-        });
-        model.addAttribute("opcoes", opcoes);
+        model.addAttribute("opcoes", Arrays.asList(Categoria.values()));
         model.addAttribute("livro", new Livro());
         return "livroCadastro";
     }
 
     @PostMapping("/cadastrar")
-    public String cadastrarLivro (Livro livro, Model model) {
+    public String cadastrarLivro (@Valid Livro livro, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("opcoes", Arrays.asList(Categoria.values()));
+            model.addAttribute("livro", livro);
+            return "livroCadastro";
+        }
         livroService.createLivro(livro);
         return listarLivros(model);
     }
