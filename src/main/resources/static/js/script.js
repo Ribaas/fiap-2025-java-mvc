faddf8192ee7415559bf133a5ec549fda0a95545
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-22 22:34:17 -0300

    aula 22/8

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+function showModal(modalID) {
+    document.getElementById(modalID).style.display = "block";
+}
+
+function hideModal(modalID) {
+  document.getElementById(modalID).style.display = "none";
+}
